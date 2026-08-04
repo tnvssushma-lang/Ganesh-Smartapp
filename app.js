39c3d3309f9f2e3bb2b54f6236e3b06404d73665
tnvssushma-lang <tnvssushma@gmail.com>
2026-08-04 16:11:03 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,108 @@
+let extractedText = "";
+
+
+function processBill() {
+
+    let file = document.getElementById("billImage").files[0];
+
+    if (!file) {
+        alert("Please select a bill image first.");
+        return;
+    }
+
+
+    document.getElementById("status").innerHTML =
+        "Reading bill... Please wait";
+
+
+    Tesseract.recognize(
+        file,
+        'eng',
+        {
+            logger: function(info) {
+
+                if (info.status === "recognizing text") {
+
+                    let progress = Math.round(info.progress * 100);
+
+                    document.getElementById("status").innerHTML =
+                    "Reading bill... " + progress + "%";
+
+                }
+
+            }
+        }
+
+    ).then(({ data: { text } }) => {
+
+
+        extractedText = text;
+
+
+        document.getElementById("billText").value = text;
+
+
+        document.getElementById("status").innerHTML =
+        "Bill reading completed";
+
+
+    })
+
+    .catch(error => {
+
+        console.log(error);
+
+        document.getElementById("status").innerHTML =
+        "Unable to read bill";
+
+    });
+
+}
+
+
+
+
+function exportExcel() {
+
+
+    if (extractedText === "") {
+
+        alert("Please convert a bill first.");
+
+        return;
+
+    }
+
+
+    let data = [
+
+        ["SmartBill AI - Extracted Bill"],
+
+        ["Details"],
+
+        [extractedText]
+
+    ];
+
+
+    let worksheet =
+        XLSX.utils.aoa_to_sheet(data);
+
+
+    let workbook =
+        XLSX.utils.book_new();
+
+
+    XLSX.utils.book_append_sheet(
+        workbook,
+        worksheet,
+        "Bill Details"
+    );
+
+
+    XLSX.writeFile(
+        workbook,
+        "SmartBill_Excel.xlsx"
+    );
+
+}
